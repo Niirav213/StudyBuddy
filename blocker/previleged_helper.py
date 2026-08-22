2b574b0f7f63bd252418e5958a6d868452f016e3
Nirav <np5662948@gmail.com>
2026-08-22 21:52:40 +0530

    completed whole website blocker part of the project

--- blocker/previleged_helper.py
+++ blocker/previleged_helper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import tempfile
 import sys
@@ -5,23 +7,23 @@ import sys
 
 
 
-def update_block_of_hosts(content:str,target_file_path:str = '/home/nirav/Projects/StudyBuddy/hosts_test.txt'):
-
-    
-    target_dir = os.path.dirname(os.path.abspath(target_file_path))
-    with tempfile.NamedTemporaryFile(mode='w+',dir=target_dir, delete=False) as tmp_file:
-        temp_path = tmp_file.name
-        tmp_file.write(content)
-
-    os.chmod(temp_path, 0o644)
+def update_block_of_hosts(content:str,target_file_path:str = '/etc/hosts'):
+    temp_path = None
     try:
+        target_dir = os.path.dirname(os.path.abspath(target_file_path))
+        with tempfile.NamedTemporaryFile(mode='w+',dir=target_dir, delete=False) as tmp_file:
+            temp_path = tmp_file.name
+            tmp_file.write(content)
+
+        os.chmod(temp_path, 0o644)
+    
         os.replace(temp_path,target_file_path)
         print("SUCCESSFULLY DONE")
     except OSError:
         print("YOU NEED ADMINISTRATOR PRIVILAGES TO REPLACE IT")
         print("TRY RUNNING IT WITH SUDO")
-
-        os.remove(temp_path)
+        if temp_path:
+            os.remove(temp_path)
         return 1
     return 0
 
