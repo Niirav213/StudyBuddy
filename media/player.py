1d7b2a1a729b128c0c3d8f88773244e258f4c7e6
Nirav <np5662948@gmail.com>
2026-09-16 10:33:30 +0530

    added last part of the youtube player module

--- media/player.py
+++ media/player.py
@@ -1,4 +1,5 @@
 import sys, subprocess
+from urllib.error import URLError
 
 class AmbiencePlayer():
     def __init__(self, target_file_path):
@@ -22,6 +23,44 @@ class AmbiencePlayer():
                 self.playing = False
             else:
                 print("nothing is playing")
+
+
+
+
+
+
+class VideoPlayer():
+    def __init__(self):
+        self.playing = False
+        self.process = None
+
+
+    def play(self, video_url:str, audio_url:str):
+
+        command = ['mpv', video_url, f'--audio-file={audio_url}', '--no-terminal']
+        try:
+            if not self.playing:
+                self.process = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
+                self.playing = True
+
+        except FileNotFoundError:
+            print("mpv is not installed please install it first")
+
+
+    def stop(self):
+        if self.process:
+            if self.process.poll() is None and self.playing:
+                self.process.kill()
+                self.process.wait()
+                self.playing = False
+
+                print('Process ended with exitCode:', self.process.returncode)
+            else:
+                print("nothing is playing")
+
+
+
+
 
 
 if __name__=="__main__":
